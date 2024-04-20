6bad19972de58da5c2b1067fc1d6999ee15a6e59
nathanielcaron <42100702+nathanielcaron@users.noreply.github.com>
2024-04-20 17:30:19 -0300

    Added data and simple bigram model

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Generatively-Pretrained-Transformer
+# Generatively Pretrained Transformer
+
+A simple generatively pre-trained transformer trained on thw works of Fyodor Dostoevsky translated in English.
+
+Based on: https://github.com/karpathy/ng-video-lecture.git
+
+All material was obtained from Project Gutenberg: https://www.gutenberg.org/
---
+++ bigram.py
@@ -0,0 +1,142 @@
+import os
+import torch
+import torch.nn as nn
+from torch.nn import functional
+
+# --- Hyper Parameters ---
+batch_size = 32 # Number of independent sequences processed in parallel
+block_size = 8 # Maximum context length for prediction
+max_iterations = 3000
+evaluation_interval = 300
+learning_rate = 1e-2
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+evaluation_iterations = 200
+# --- Hyper Parameters ---
+
+torch.manual_seed(1337)
+
+def read_data(directory):
+    contents = ''
+
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+
+        if os.path.isfile(filepath):
+            with open(filepath, 'r', encoding='utf-8') as file:
+                content = file.read()
+                contents += content
+
+    return contents
+
+contents = read_data('Data')
+
+# Unique characters in data
+unique_characters = sorted(list(set(contents)))
+vocabulary_size = len(unique_characters)
+
+print('Unique characters in file contents: ', unique_characters)
+print('Vocabulary size: ', vocabulary_size)
+
+# Mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(unique_characters) }
+itos = { i:ch for i,ch in enumerate(unique_characters) }
+
+# Encoder: string to integers
+encode = lambda s: [stoi[c] for c in s]
+# Decoder: Integers to string
+decode = lambda l: ''.join([itos[i] for i in l])
+
+data = torch.tensor(encode(contents), dtype=torch.long)
+# First 90% of data is training data, last 10% is validation data
+n = int(0.9*len(data))
+training_data = data[:n]
+validation_data = data[n:]
+
+# Load data
+def get_batch(split):
+    # Batch of data for inputs x and targets y
+    data = training_data if split == 'train' else validation_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(evaluation_iterations)
+        for k in range(evaluation_iterations):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+# Simple bigram model
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        # Each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx) # (B,T,C)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = functional.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # Get predictions
+            logits, loss = self(idx)
+            # Focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # Apply softmax to get probabilities
+            probs = functional.softmax(logits, dim=-1) # (B, C)
+            # Sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # Append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+
+model = BigramLanguageModel(vocabulary_size)
+m = model.to(device)
+
+# create a PyTorch optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iterations):
+
+    # Evaluate the loss on trining and validation sets
+    if iter % evaluation_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: training loss {losses['train']:.4f}, validation loss {losses['val']:.4f}")
+
+    # Sample a batch of data
+    xb, yb = get_batch('train')
+
+    # Evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+# Generate from the model
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
